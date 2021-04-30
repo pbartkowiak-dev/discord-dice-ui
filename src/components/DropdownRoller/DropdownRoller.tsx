9825e121c9426b4ed9cbc8e1bf760ad318a6713b
Przemyslaw Bartkowiak <pbartkowiak@fandom.com>
2021-04-30 09:49:19 +0200

    Add DropdownRoller second level menu

--- src/components/DropdownRoller/DropdownRoller.tsx
+++ src/components/DropdownRoller/DropdownRoller.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dropdown } from "react-bootstrap";
 import classNames from "classnames";
 import { commonDice } from '../../consts/diceSets';
@@ -11,34 +11,79 @@ interface DropdownRoller {
 }
 
 function DropdownRoller({ onToggle } : DropdownRoller) {
+	const [show, setShow] = useState(false);
 	const dispatch = useDispatch();
-	// @TODO MOVE TO UTILS/CREATE HOOK
+
 	const handleRollDice = (diceType: string, diceAmount: number = 1) => {
+		setShow(false);
+		onToggle(false);
+
 		dispatch(submitRoll({
 			diceType,
 			diceAmount
 		}));
-	}
+	};
+
+	const toggle = () => {
+		onToggle(!show);
+		setShow(!show);
+	};
 
 	return (
-		<Dropdown drop={"up"} onToggle={onToggle}>
+		<Dropdown drop={"up"} show={show}>
 			<Dropdown.Toggle
 				variant="outline-primary"
 				id="dropdown-roller"
+				onClick={toggle}
 			>
-				<span>Roll the dice</span>
+				<span>Roll the Dice</span>
 			</Dropdown.Toggle>
 			<Dropdown.Menu>
-				<div className=""> {
+				<div> {
 					commonDice.map(({ diceType, label }) => (
 						<Dropdown.Item
 							key={diceType}
 							href="#"
-							onClick={(e) => handleRollDice(diceType)}>
-							<div>
-								<img src={require(`../../img/${diceType}.png`)} className={styles.dieImg }/>
-								Roll <strong>{label}</strong>
-							</div>
+						>
+							<Dropdown drop={"right"}>
+								<Dropdown.Toggle
+									as="div"
+									id="dropdown-roller-second-level"
+								>
+									<span>
+										<img
+											src={require(`../../img/${diceType}.png`)}
+											className={styles.dieImg}
+											alt={diceType}
+										/>
+										<strong>{label}</strong>
+									</span>
+								</Dropdown.Toggle>
+								<Dropdown.Menu>
+									<div> {
+										[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((diceAmount) => (
+											<>
+												<Dropdown.Item
+													key={diceAmount}
+													href="#"
+													onClick={(e) => handleRollDice(diceType, diceAmount)}>
+													<span>
+														<img
+															src={require(`../../img/${diceType}.png`)}
+															className={styles.dieImgSecond}
+															alt={diceType}
+														/>
+														Roll <strong>{diceAmount > 1 ? diceAmount : ''}{label}</strong>
+													</span>
+												</Dropdown.Item>
+												{ diceAmount === 1 && <Dropdown.Divider /> }
+											</>
+										))
+									} </div>
+								</Dropdown.Menu>
+							</Dropdown>
+
+
 						</Dropdown.Item>
 					))
 				} </div>
