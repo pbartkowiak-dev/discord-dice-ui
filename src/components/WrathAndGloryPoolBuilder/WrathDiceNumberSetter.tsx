688d73fb441eddbacbff8fbc174ace0fc198926a
Przemyslaw Bartkowiak <pbartkowiak@fandom.com>
2021-08-28 17:12:29 +0200

    Refactor Wrath Dice Number Setter component

--- src/components/WrathAndGloryPoolBuilder/WrathDiceNumberSetter.tsx
+++ src/components/WrathAndGloryPoolBuilder/WrathDiceNumberSetter.tsx
@@ -1,86 +1,23 @@
-import React from 'react';
-import classNames from 'classnames';
+import React, { FC } from 'react';
 import useWrathAndGloryStore from "./store";
-import { faSkull } from "@fortawesome/free-solid-svg-icons";
-import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import styles from "./WrathDiceNumberSetter.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CodeSpan from "../CodeSpan/CodeSpan";
+import WrathDieSelector from "./WrathDieSelector";
 
-function WrathDiceNumberSetter() {
+const WrathDiceNumberSetter: FC = () => {
 	const wrathDiceNumber = useWrathAndGloryStore(({ wrathDiceNumber }) => wrathDiceNumber);
-	const setWrathDiceNumber = useWrathAndGloryStore(({ setWrathDiceNumber }) => setWrathDiceNumber);
+	const maxWrathDice = 7;
 
 	return (
 		<div className={styles.container}>
-			<span>Set Wrath Dice Number (<CodeSpan>{wrathDiceNumber}</CodeSpan>):</span><ul className={styles.iconsRow}>
-			<li className={classNames({
-				[styles.iconContainer]: true,
-				[styles.iconSelected]: wrathDiceNumber === 0,
-			})}
-				onClick={() => setWrathDiceNumber(0)}
-			>
-				<FontAwesomeIcon icon={faTimesCircle} />
-			</li>
-			<li className={classNames({
-				[styles.iconContainer]: true,
-				[styles.iconSelected]: wrathDiceNumber >= 1
-			})}
-				onClick={() => setWrathDiceNumber(1)}
-			>
-				<FontAwesomeIcon icon={faSkull} />
-			</li>
-			<li className={classNames({
-				[styles.iconContainer]: true,
-				[styles.iconSelected]: wrathDiceNumber >= 2
-			})}
-				onClick={() => setWrathDiceNumber(2)}
-			>
-				<FontAwesomeIcon icon={faSkull} />
-			</li>
-			<li className={classNames({
-				[styles.iconContainer]: true,
-				[styles.iconSelected]: wrathDiceNumber >= 3
-			})}
-				onClick={() => setWrathDiceNumber(3)}
-			>
-				<FontAwesomeIcon icon={faSkull} />
-			</li>
-			<li className={classNames({
-			[styles.iconContainer]: true,
-				[styles.iconSelected]: wrathDiceNumber >= 4
-			})}
-				onClick={() => setWrathDiceNumber(4)}
-			>
-				<FontAwesomeIcon icon={faSkull} />
-			</li>
-			<li className={classNames({
-				[styles.iconContainer]: true,
-				[styles.iconSelected]: wrathDiceNumber >= 5
-			})}
-				onClick={() => setWrathDiceNumber(5)}
-			>
-				<FontAwesomeIcon icon={faSkull} />
-			</li>
-			<li className={classNames({
-				[styles.iconContainer]: true,
-				[styles.iconSelected]: wrathDiceNumber >= 6
-			})}
-				onClick={() => setWrathDiceNumber(6)}
-			>
-				<FontAwesomeIcon icon={faSkull} />
-			</li>
-			<li className={classNames({
-				[styles.iconContainer]: true,
-				[styles.iconSelected]: wrathDiceNumber >= 7
-			})}
-				onClick={() => setWrathDiceNumber(7)}
-			>
-				<FontAwesomeIcon icon={faSkull} />
-			</li>
-		</ul>
+			<span>Set Wrath Dice Number (<CodeSpan>{wrathDiceNumber}</CodeSpan>):</span>
+			<ul className={styles.iconsRow}>
+				{ new Array(maxWrathDice).fill('.').map((_: string, index) => (
+					<WrathDieSelector wrathDieValue={index} />
+				))}
+			</ul>
 		</div>
 	);
-}
+};
 
-export default WrathDiceNumberSetter;
+export default WrathDiceNumberSetter
---
+++ src/components/WrathAndGloryPoolBuilder/WrathDieSelector.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import useWrathAndGloryStore from "./store";
+import classNames from "classnames";
+import styles from "./WrathDiceNumberSetter.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSkull, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+
+interface WrathDieProps {
+	wrathDieValue: number
+}
+
+const WrathDieSelector: FC<WrathDieProps> = ({ wrathDieValue }) => {
+	const setWrathDiceNumber = useWrathAndGloryStore(({ setWrathDiceNumber }) => setWrathDiceNumber);
+	const wrathDiceNumber = useWrathAndGloryStore(({ wrathDiceNumber }) => wrathDiceNumber);
+
+	return (
+		<li className={classNames({
+			[styles.iconContainer]: true,
+			[styles.iconSelected]: wrathDiceNumber >= wrathDieValue
+		})}
+			onClick={() => setWrathDiceNumber(wrathDieValue)}
+		>
+			{ wrathDieValue === 0
+				? <FontAwesomeIcon icon={faTimesCircle} />
+				: <FontAwesomeIcon icon={faSkull}/>
+			}
+		</li>
+	);
+};
+
+export default WrathDieSelector;
