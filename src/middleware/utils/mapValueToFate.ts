aae419dab26d59368783d98051c96d6af60af24f
Przemyslaw Bartkowiak <pbartkowiak@fandom.com>
2021-10-30 21:38:42 +0200

    allow rolling selected Fate dice

--- src/middleware/utils/mapValueToFate.ts
+++ src/middleware/utils/mapValueToFate.ts
@@ -1,16 +1,10 @@
-import { PLUS, MINUS, BLANK } from '../../consts/fateConsts';
+import { PLUS, MINUS, BLANK, FateResult } from '../../consts/fateConsts';
 
-export default (value: number): string => {
-	switch (value) {
-		case 1:
-		case 2:
-			return PLUS;
-		case 3:
-		case 4:
-			return BLANK;
-		case 5:
-		case 6:
-			return MINUS;
+export default (value: number): FateResult => {
+	if (value === 1 || value === 2) {
+		return PLUS;
+	} else if (value === 3 || value === 4) {
+		return BLANK;
 	}
-	return `${value}`;
+	return MINUS;
 };
