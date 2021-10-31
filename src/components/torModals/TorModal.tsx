1b189aab17751de14e15e48cb464232203205d1a
Przemyslaw Bartkowiak <pbartkowiak@fandom.com>
2021-10-31 16:11:33 +0100

    add TN input to TOR modal

--- src/components/torModals/TorModal.tsx
+++ src/components/torModals/TorModal.tsx
@@ -1,34 +1,48 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from "react-redux";
 import classNames from 'classnames';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import styles from "../ResultsModal/ResultsModal.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDiceD20 } from "@fortawesome/free-solid-svg-icons";
-import { closeCthulhuResultsModal } from "../../actions/cthulhu.actions";
-import ResultVsSkillRow from "../ResultVsSkillRow/ResultVsSkillRow";
-import SuccessLevelLadder from "../SuccessLevelLadder/SuccessLevelLadder";
-import joinAsBlocks from "../../utils/joinAsBlocks";
-import torStyles from "../ResultsModal/ResultsModal.module.css";
+import torStyles from "./TorModal.module.css";
 import useTorStore from "../tor/store";
 import InputRange from "../InputRange/InputRange";
 import Form from "react-bootstrap/Form";
+import InfoTooltip from "../InfoTooltip/InfoTooltip";
+
 
 function TorModal() {
-	const dispatch = useDispatch();
 	const torState = useTorStore((torState: any) => torState);
-	// const lastRollOptions = useSelector(({ lastRollOptions }: any) => lastRollOptions);
-	const { isModalOpen, closeModal, isSuccess, results } = torState;
+	const { isModalOpen, closeModal } = torState;
 
 	const tnInputId = 'tn-input'
-	const [tn, setTn] = useState<string>();
-	const onTnRangeChange = (event: React.ChangeEvent<HTMLInputElement>) => setTn(event.target.value);
+	const tnMax = 20;
+	const tnMin = 0;
+	const [tn, setTn] = useState<string>('');
+
+	const isValid = () => {
+		let isValid = true;
+		const tnNumber = Number(tn);
+
+		if (isNaN(tnNumber) || tnNumber <= 0 || tnNumber > tnMax) {
+			isValid = false;
+		}
+
+		return isValid;
+	}
+
+	const onTnRangeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setTn(event.target.value);
+	};
 
 	const onTnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const inputRange = document.getElementById(tnInputId) as HTMLInputElement;
-		if (inputRange) {
-			inputRange.value = event.target.value;
+		const { value } = event.target;
+		const valueNumber = Number(value);
+
+		if (valueNumber <= tnMax && !isNaN(valueNumber) && valueNumber >= tnMin) {
+			if (inputRange) {
+				inputRange.value = value;
+			}
+			setTn(value);
 		}
 	};
 
@@ -37,30 +51,29 @@ function TorModal() {
 			<Modal.Header closeButton>
 				<Modal.Title>The One Ring 2e Roll Options</Modal.Title>
 			</Modal.Header>
-			<Modal.Body>
-				<Form.Group>
-					<div className="percent-icon-container">
-						<Form.Control
-							type="text"
-							size="lg"
-							placeholder="TN"
-							autoComplete="off"
-							onChange={onTnChange}
-							value={tn}
+			<Modal.Body className={torStyles.torModalBody}>
+				<div className={torStyles.tnContainer}>
+					<InfoTooltip
+						content="Target Number"
+						className={torStyles.tnInfoIcon}
+					/>
+					<Form.Control
+						type="text"
+						size="lg"
+						placeholder="TN"
+						autoComplete="off"
+						onChange={onTnChange}
+						value={tn}
+						className={torStyles.tnInput}
 
-							// isInvalid ={hasError}
-							//{...input}
-						/>
-					</div>
-					{/*{ hasError && <Form.Control.Feedback type="invalid">{ error }</Form.Control.Feedback> }*/}
-					{/*{ textMuted && <Form.Text className="text-muted">{ textMuted }</Form.Text> }*/}
-				</Form.Group>
+					/>
+				</div>
 				<InputRange
 					id={tnInputId}
 					onChange={onTnRangeChange}
 					hidePercent={true}
-					max={20}
-					min={1}
+					max={tnMax}
+					min={tnMin}
 				/>
 			</Modal.Body>
 			<Modal.Footer>
@@ -70,6 +83,7 @@ function TorModal() {
 				<Button
 					variant="success"
 					type="submit"
+					disabled={!isValid()}
 					>Roll!
 				</Button>
 			</Modal.Footer>
