d9d9068d996b30adbc194902c2a13d244e89181d
Przemyslaw Bartkowiak <pbartkowiak@fandom.com>
2021-11-02 19:30:45 +0100

    Add missing tooltips to the TOR modal

--- src/components/torModals/TorModal.tsx
+++ src/components/torModals/TorModal.tsx
@@ -109,7 +109,14 @@ export default () => {
 						type="checkbox"
 						name="Favoured Roll"
 						id="Favoured Roll"
-						label="Favoured Roll"
+						label={(
+							<div>
+								<span>Favoured Roll</span>
+								<InfoTooltip content={(
+									<span>When a roll is <strong>Favoured</strong>, players roll two Feat Dice instead of one, keeping the best result.</span>
+								)} />
+							</div>
+						)}
 						checked={isFavoured}
 						onChange={() => setIsFavoured(!isFavoured)}
 						disabled={isIllFavoured}
@@ -119,7 +126,14 @@ export default () => {
 						type="checkbox"
 						name="Ill-Favoured Roll"
 						id="Ill-Favoured Roll"
-						label="Ill-Favoured Roll"
+						label={(
+							<div>
+								<span>Ill-Favoured Roll</span>
+								<InfoTooltip content={(
+									<span>When a roll is <strong>Ill-Favoured</strong>, players roll two Feat Dice instead of one, keeping the worst result.</span>
+								)} />
+							</div>
+						)}
 						checked={isIllFavoured}
 						onChange={() => setIsIllFavoured(!isIllFavoured)}
 						disabled={isFavoured}
@@ -167,7 +181,14 @@ export default () => {
 						type="checkbox"
 						name="Weary"
 						id="Weary"
-						label="Weary"
+						label={(
+							<div>
+								<span>Weary</span>
+								<InfoTooltip content={(
+									<span>When a hero is <strong>Weary</strong>, all the Success Dice showing <strong>1</strong>, <strong>2</strong>, or <strong>3</strong> are considered as a result of <strong>0</strong> instead.</span>
+								)} />
+							</div>
+						)}
 						checked={isWeary}
 						onChange={() => setIsWeary(!isWeary)}
 						custom
@@ -176,7 +197,14 @@ export default () => {
 						type="checkbox"
 						name="Miserable"
 						id="Miserable"
-						label="Miserable"
+						label={(
+							<div>
+								<span>Miserable</span>
+								<InfoTooltip content={(
+									<span>When a hero is <strong>Miserable</strong>, the roll is considered a Failure if a Feat Die shows the <FontAwesomeIcon icon={faEye} /> icon (<strong>{EYE_SCORE}</strong>), regardless of the total result obtained by the roll.</span>
+								)} />
+							</div>
+						)}
 						checked={isMiserable}
 						onChange={() => setIsMiserable(!isMiserable)}
 						custom
@@ -194,9 +222,7 @@ export default () => {
 						custom
 					/>
 					<InfoTooltip content={(
-						<span>
-							When rolling for <strong>an Adversary</strong>, the <FontAwesomeIcon icon={faEye} /> icon (<strong>{EYE_SCORE}</strong>) becomes the highest result possible, while the <GandalfRune  style={{ fill: '#fff' }} /> rune (<strong>{GANDALF_SCORE}</strong>) becomes the lowest result possible and is read&nbsp;as&nbsp;<strong>0</strong>.
-						</span>
+						<span>When rolling for <strong>an Adversary</strong>, the <FontAwesomeIcon icon={faEye} /> icon (<strong>{EYE_SCORE}</strong>) becomes the highest result possible, while the <GandalfRune  style={{ fill: '#fff' }} /> rune (<strong>{GANDALF_SCORE}</strong>) becomes the lowest result possible and is read&nbsp;as&nbsp;<strong>0</strong>.</span>
 					)} />
 				</div>
 			</Modal.Body>
