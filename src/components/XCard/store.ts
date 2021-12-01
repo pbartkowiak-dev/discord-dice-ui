5e421ba1c33174251120954ccb2678b5a81a56c4
Przemyslaw Bartkowiak <pbartkowiak@fandom.com>
2021-12-01 20:21:10 +0100

    Add store

---
+++ src/components/XCard/store.ts
@@ -0,0 +1,41 @@
+import create from 'zustand';
+import { requestMsgReady } from "../../actions/roll.actions";
+import reduxStore from '../../store';
+import { getDiscordMsgData } from "./getDiscordMsgData";
+
+export type State = {
+	openModal: () => void;
+	isModalOpen: boolean;
+	closeModal: () => void;
+	throwXCard: () => void;
+	isAnonymous: boolean;
+	setIsAnonymous: (isAnonymous: boolean) => void;
+}
+
+const useStore = create<State>(((set, get) => ({
+	isModalOpen: false,
+	isAnonymous: true,
+
+	openModal: () => set({
+		isModalOpen: true,
+		isAnonymous: true,
+	}),
+
+	closeModal: () => set({
+		isModalOpen: false
+	}),
+
+	setIsAnonymous: (isAnonymous) => set({
+		isAnonymous
+	}),
+
+	throwXCard() {
+		const {	isAnonymous } = get();
+
+		reduxStore.dispatch(requestMsgReady(
+			getDiscordMsgData({ isAnonymous })
+		));
+	},
+})));
+
+export default useStore;
