a84b16353bb76d780fb4f8ce041feac418f8b807
Przemyslaw Bartkowiak <przemyslaw.bartkowiak@sonalake.com>
2022-04-19 15:55:28 +0200

    Use universal Tokens Modal in place of hardcoded ones

--- src/components/InfinityTokensModal/InfinityTokensModalContainer.tsx
+++ src/components/InfinityTokensModal/InfinityTokensModalContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { hideMsg } from "../../actions/modals";
 import { updateTokensState } from "../../actions/infinity.actions";
-import InfinityTokensModal from "./InfinityTokensModal";
+import { TokensModal, TokensState } from "../tokens-modal/tokens-modal";
 
 const mapStateToProps = (state: any) => {
   const { infinityData } = state;
@@ -21,13 +21,34 @@ function InfinityTokensModalContainer({
   hideMsg,
   infinityData: { showTokensModal, momentum, heat },
 }: any) {
+  const handleUpdateTokens = (tokensState: TokensState) => {
+    const { tokenOneState, tokenTwoState } = tokensState;
+
+    updateTokensState({
+      heat: tokenOneState,
+      momentum: tokenTwoState,
+    });
+  };
+
   return (
-    <InfinityTokensModal
-      updateTokensState={updateTokensState}
+    <TokensModal
       showModal={showTokensModal}
       hideMsg={hideMsg}
-      momentum={momentum}
-      heat={heat}
+      handleUpdateTokens={handleUpdateTokens}
+      tokenOne={{
+        name: "Heat",
+        initialValue: heat,
+        img: "infinity_heat.png",
+        min: 0,
+        max: 30,
+      }}
+      tokenTwo={{
+        name: "Momentum",
+        initialValue: momentum,
+        img: "infinity_momentum.png",
+        min: 0,
+        max: 6,
+      }}
     />
   );
 }
