8ba033d66f87b36ed0e799b4b13f8809e0b7b2b4
Przemyslaw Bartkowiak <przemyslaw.bartkowiak@sonalake.com>
2022-04-19 17:46:27 +0200

    Add Dune Pool tracker

---
+++ src/reducers/2d20-types.ts
@@ -0,0 +1,20 @@
+export interface InfinityState {
+  showTokensModal: boolean;
+  momentum: string;
+  heat: string;
+  assistanceDiceResults: Array<number>;
+}
+
+export interface ConanState {
+  showTokensModal: boolean;
+  momentum: string;
+  doom: string;
+  assistanceDiceResults: Array<number>;
+}
+
+export interface DuneState {
+  showTokensModal: boolean;
+  momentum: string;
+  threat: string;
+  assistanceDiceResults: Array<number>;
+}
